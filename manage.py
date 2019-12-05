6fc09061b58cf86a846a4d881ddc022b39ca5b18
Eloka Chima <eloka.chima@gmail.com>
2019-12-05 04:05:03 +0100

    modify manage file

--- manage.py
+++ manage.py
@@ -11,17 +11,18 @@ if os.environ['ENV'] in ['PRODUCTION']:
 else:
     setting = 'hotel_api.settings.development'
 
-def main():
+if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', setting)
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+    except ImportError:
+        try:
+            import django
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
+        raise
     execute_from_command_line(sys.argv)
-
-if __name__ == '__main__':
-    main()
